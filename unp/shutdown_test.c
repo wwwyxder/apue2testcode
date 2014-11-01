aab40ed25217e956ebe73769ab8b2f0f22ea0d57
CentOS <wwwyxder@sina.cn>
2014-11-01 21:49:00 +0800

    unp

---
+++ unp/shutdown_test.c
@@ -0,0 +1,18 @@
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include "unpheader.h"
+int main(int argc, char *argv[])
+{
+    int clifd = Socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+    servaddr.sin_port = htons(12200);
+    Connect(clifd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+    sleep(1);
+    shutdown(clifd,SHUT_RD);
+    shutdown(clifd,SHUT_WR);
+    sleep(2);
+    return 0;
+}
