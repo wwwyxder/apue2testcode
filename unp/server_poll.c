97d359fee317af5cdb463badeddae3f85812ffa8
yxd <wwwyxder@sina.cn>
2014-11-08 01:52:42 +0800

    sin_family

--- unp/server_poll.c
+++ unp/server_poll.c
@@ -7,6 +7,7 @@ int main(int argc, char* argv[])
 {
     struct sockaddr_in addr;
     memset(&addr,0,sizeof(addr));
+    addr.sin_family = AF_INET;
     if(argc==1) {
         addr.sin_addr.s_addr = htonl(INADDR_ANY);
     } else {
